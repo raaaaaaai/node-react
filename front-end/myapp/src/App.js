a1d2da03c610b5671a347f0b54b3f6d89ccf6e36
Rai Muhammad Awais <96009190+raaaaaaai@users.noreply.github.com>
2023-08-11 10:44:07 +0500

    Update App.js

--- front-end/myapp/src/App.js
+++ front-end/myapp/src/App.js
@@ -1,28 +1,37 @@
-import React ,{useEffect,useState} from 'react'
-function App(){
-  const [backendData,setBackendData] = useState([{}]) 
+import React, { useEffect, useState } from 'react';
 
-  useEffect(()=>{
-    fetch("/api").then(
-       response => response.json()
-    ).then(
-        data =>{
-          setBackendData(data)
-        }
-    )
-  },[])
+function App() {
+  const [backendData, setBackendData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div>
-        { (typeof backendData.users === 'undefined') ? (
-             <p>loading..</p>
-        ) :(
-          backendData.users.map((user,i)=>(
-            <p key={i}>{user}</p>
-          ))
-        )
-          }
+      <button onClick={fetchData}>Bring Data from Backend</button>
+      <div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            {backendData.users ? (
+              backendData.users.map((user, i) => <p key={i}>{user}</p>)
+            ) : (
+              <p>No data available.</p>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
